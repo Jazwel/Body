9ae3f04be249687bb2a813111527ca8bc79456e6
DS <mc_sd@bk.ru>
2023-04-12 22:13:21 -0300

    Body

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        BodyMassIndex service = new BodyMassIndex;
+        BmiService service = new BmiService ();
+
+        double index = service.calculate(98,1.87);
+
+        System.out.println(index);
 
 
     }
